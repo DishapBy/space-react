841c246502de32faf62bc6060465fa3147d7de81
Dmitry <dishapby@gmail.com>
2020-10-14 14:02:37 +0300

    new components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import logo from './logo.svg';
 import './style.css';
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
+import Footer from "./components/Footer/Footer";
 
 function App() {
     return (
         <React.Fragment>
-            <Header />
             <Main />
+            <Footer />
         </React.Fragment>
     )
 }
