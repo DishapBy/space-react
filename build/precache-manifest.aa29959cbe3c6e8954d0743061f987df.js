d687fe63a4246e9b559e90f7126e3a7a49c72392
Dmitry <dishapby@gmail.com>
2020-10-18 20:13:43 +0300

    depl

---
+++ build/precache-manifest.aa29959cbe3c6e8954d0743061f987df.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9acfc52be7623ea829513d73a1f49c27",
+    "url": "/DishapBy/space-react/index.html"
+  },
+  {
+    "revision": "6a78eb4e07267e9339c9",
+    "url": "/DishapBy/space-react/static/css/main.79e90019.chunk.css"
+  },
+  {
+    "revision": "c841982f661cd5d49647",
+    "url": "/DishapBy/space-react/static/js/2.2c72a448.chunk.js"
+  },
+  {
+    "revision": "8a9adb48f7af2062183ac70b4922c4ef",
+    "url": "/DishapBy/space-react/static/js/2.2c72a448.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a78eb4e07267e9339c9",
+    "url": "/DishapBy/space-react/static/js/main.6271299f.chunk.js"
+  },
+  {
+    "revision": "afaf706557a53ffdaec9",
+    "url": "/DishapBy/space-react/static/js/runtime-main.6a33aafe.js"
+  },
+  {
+    "revision": "6160d2728a6ae1625a2e4bf7fbdc0e33",
+    "url": "/DishapBy/space-react/static/media/earth.6160d272.mp4"
+  },
+  {
+    "revision": "4e43f38ece9edcac9d2f6cf4a21e5ae1",
+    "url": "/DishapBy/space-react/static/media/falcon1.4e43f38e.png"
+  },
+  {
+    "revision": "70c0d6ca57039f250f7a46da490536e7",
+    "url": "/DishapBy/space-react/static/media/falcon9.70c0d6ca.png"
+  },
+  {
+    "revision": "c1e42c98b18c115c2219abbfd98b9dd4",
+    "url": "/DishapBy/space-react/static/media/falconHeavy.c1e42c98.png"
+  },
+  {
+    "revision": "d5ab31ce033fc418fd1af87f7b934e1b",
+    "url": "/DishapBy/space-react/static/media/logo.d5ab31ce.svg"
+  },
+  {
+    "revision": "50a0c44519288f319f171b2250c40cb3",
+    "url": "/DishapBy/space-react/static/media/mars.50a0c445.mp4"
+  },
+  {
+    "revision": "c576729b41d649227eb2a4afff68ae39",
+    "url": "/DishapBy/space-react/static/media/moon.c576729b.mp4"
+  },
+  {
+    "revision": "7a69e90456d80e1fe0d1b4740e83dcf2",
+    "url": "/DishapBy/space-react/static/media/space.7a69e904.mp4"
+  },
+  {
+    "revision": "9502c3707c7d2debb7ddf433f6d28e39",
+    "url": "/DishapBy/space-react/static/media/starship.9502c370.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b7ef2aaa82aab92d1fd55324b9e68f13.js"
+  "/DishapBy/space-react/precache-manifest.aa29959cbe3c6e8954d0743061f987df.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DishapBy/space-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
